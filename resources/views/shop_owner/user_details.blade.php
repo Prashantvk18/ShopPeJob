4aad53769046f19533039c9a856e4b7a94bde64f
Prashantvk18 <prashantjainvk18@gmail.com>
2025-05-30 19:15:41 +0530

    'shop_side'

--- resources/views/shop_owner/user_details.blade.php
+++ resources/views/shop_owner/user_details.blade.php
@@ -16,88 +16,91 @@
         <div class="col-md-10 col-lg-8">
             <div class="card shadow-sm border border-dark h-100" style="border-radius: 10px;">
                 <div class="card-body">
-                <div class="d-flex justify-content-center">
-                    <div class="profile-pic-wrapper">
-                        <img src="{{ $profilePhoto }}" alt="Profile Photo">
+                    <div class="col-md-6 ">
+                   
+
+                        
+                        <p class="card-text mb-3">
+                            <i class="fa fa-user me-2 text-secondary"></i>
+                            <strong>Name:</strong> {{ $user_data->first_name }} {{ $user_data->middle_name }} {{ $user_data->last_name }}
+                        </p>
+                        <p class="card-text mb-3">
+                            <i class="fa fa-map-marker text-danger me-2"></i>
+                            &nbsp;<strong>Location:</strong>
+                            @foreach($states as $state)
+                            @if($state->id == $user_data->state)
+                                {{ $state->name}}
+                            @endif
+                            @endforeach  , {{ $user_data->city }}
+                            <br>
+                            &nbsp; &nbsp; &nbsp;{{$user_data->address}}
+                        </p>
+
+                        <p class="card-text mb-3">
+                            <i class="fa fa-briefcase text-info me-2"></i>
+                            <strong>Education:</strong> <span class="ms-2">{{ $education[$user_data->education] }} </span>
+                        </p>
+
+
+                        <p class="card-text mb-3">
+                            <i class="fa fa-inr text-success me-2"></i>&nbsp;
+                            <strong>Salary Expectation:</strong> ₹{{ $user_data->salary_expect }}/month
+                        </p>
+
+                        <p class="card-text mb-3">
+                            <i class="fa fa-briefcase text-info me-2"></i>
+                            <strong>Work Experiance:</strong> <span class="ms-2">{{ $exp_array[$user_data->work_experience] }} </span>
+                        </p>
+
+                        <p class="card-text mb-4">
+                            <i class="fa fa-phone me-2 text-success"></i>
+                            <strong>Phone:</strong>
+                            <span class="ms-2">{{$user_data->mobile_no }}</span>
+                        </p>
+                        <div class="mb-3">
+                            <form id="job_status">
+                                @csrf
+                                <div class="col-md-6">
+                                    <input type="text" style="display:none" value="{{$apply_job_id->id}}" id="jobid"> 
+                                    <button type="submit" class="btn btn-success w-100" name="status"  onclick="update_status('S');" >Accept</button>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <button type="submit" class="btn btn-danger w-100" name="status" onclick="update_status('R');" >Reject</button>
+                                    
+                                </div>
+                            </form>
+                        </div>
+                        <hr>
                     </div>
-                </div>
-                @if($apply_job_id->status == 'S')
-                    <div style="position: absolute; top: 10px; right: 10px; transform: rotate(-10deg);">
-                        <span class="badge bg-success text-white p-2" style="font-size: 1rem; border: 2px solid #28a745;">
-                            ✅ Accepted
+                    <div class="col-md-6 ">
+                    @if($apply_job_id->status == 'S')
+                        <div style="position: absolute; top: 10px; right: 10px; transform: rotate(-10deg);">
+                            <span class="badge bg-success text-white p-2" style="font-size: 1rem; border: 2px solid #28a745;">
+                                ✅ Accepted
+                            </span>
+                        </div>
+                    @endif
+                    @if($apply_job_id->status == 'R')
+                        <div style="position: absolute; top: 10px; right: 10px; transform: rotate(-10deg);">
+                        <span class="badge bg-danger text-white p-2" style="font-size: 1rem; border: 2px solid #dc3545;">
+                        ❌ Rejected
                         </span>
-                    </div>
-                @endif
-                @if($apply_job_id->status == 'R')
-                    <div style="position: absolute; top: 10px; right: 10px; transform: rotate(-10deg);">
-                    <span class="badge bg-danger text-white p-2" style="font-size: 1rem; border: 2px solid #dc3545;">
-                    ❌ Rejected
-                    </span>
-                    </div>
-                @endif
-                @if($apply_job_id->status == null || $apply_job_id->status == 'P')
-                    <div style="position: absolute; top: 10px; right: 10px; transform: rotate(-10deg);">
-                        <span class="badge bg-warning text-dark p-2" style="font-size: 1rem; border: 2px solid #ffc107;">
-                            ⏳ Pending
-                        </span>
-                    </div>
-                @endif
-
-                    
-                    <p class="card-text mb-3">
-                        <i class="fa fa-user me-2 text-secondary"></i>
-                        <strong>Name:</strong> {{ $user_data->first_name }} {{ $user_data->middle_name }} {{ $user_data->last_name }}
-                    </p>
-                    <p class="card-text mb-3">
-                        <i class="fa fa-map-marker text-danger me-2"></i>
-                        &nbsp;<strong>Location:</strong>
-                        @foreach($states as $state)
-                        @if($state->id == $user_data->state)
-                            {{ $state->name}}
-                        @endif
-                        @endforeach  , {{ $user_data->city }}
-                        <br>
-                        &nbsp; &nbsp; &nbsp;{{$user_data->address}}
-                    </p>
-
-                    <p class="card-text mb-3">
-                        <i class="fa fa-briefcase text-info me-2"></i>
-                        <strong>Education:</strong> <span class="ms-2">{{ $education[$user_data->education] }} </span>
-                    </p>
-
-
-                    <p class="card-text mb-3">
-                        <i class="fa fa-inr text-success me-2"></i>&nbsp;
-                        <strong>Salary Expectation:</strong> ₹{{ $user_data->salary_expect }}/month
-                    </p>
-
-                    <p class="card-text mb-3">
-                        <i class="fa fa-briefcase text-info me-2"></i>
-                        <strong>Work Experiance:</strong> <span class="ms-2">{{ $exp_array[$user_data->work_experience] }} </span>
-                    </p>
-
-                    <p class="card-text mb-4">
-                        <i class="fa fa-phone me-2 text-success"></i>
-                        <strong>Phone:</strong>
-                        <span class="ms-2">{{$user_data->mobile_no }}</span>
-                    </p>
-                    <div class="mb-3">
-                        <form id="job_status">
-                            @csrf
-                            <div class="col-md-6">
-                                <input type="text" style="display:none" value="{{$apply_job_id->id}}" id="jobid"> 
-                                <button type="submit" class="btn btn-success w-100" name="status"  onclick="update_status('S');" >Accept</button>
+                        </div>
+                    @endif
+                    @if($apply_job_id->status == null || $apply_job_id->status == 'P')
+                        <div style="position: absolute; top: 10px; right: 10px; transform: rotate(-10deg);">
+                            <span class="badge bg-warning text-dark p-2" style="font-size: 1rem; border: 2px solid #ffc107;">
+                                ⏳ Pending
+                            </span>
+                        </div>
+                    @endif
+                        <div class="d-flex justify-content-center">
+                            <div class="profile-pic-wrapper">
+                                <img src="{{ $profilePhoto }}" alt="Profile Photo">
                             </div>
-
-                            <div class="col-md-6">
-                                <button type="submit" class="btn btn-danger w-100" name="status" onclick="update_status('R');" >Reject</button>
-                                
-                            </div>
-                        </form>
+                        </div>
                     </div>
-                    <hr>
-                    
-                    
                 </div>
             </div>
         </div>
